a5d2c164ba754522fedbb22088e890d51d1433fe
David Petrie <david@iMac27-2.local>
2010-05-02 21:12:05 +1200

    new header files

---
+++ hdk/inc/hdLevelEditor.h
@@ -0,0 +1,16 @@
+/*
+ *  hdLevelEditor.h
+ *  hdkOSX
+ *
+ *  Created by David Petrie on 29/04/10.
+ *  Copyright 2010 n/a. All rights reserved.
+ *
+ */
+
+#ifndef HD_LEVEL_EDITOR_H
+#define HD_LEVEL_EDITOR_H
+
+//#include "hdLevelEditor/hdLevelEditorController.h"
+//#include "hdLevelEditor/hdLevelEditorUtilities.h"
+
+#endif
---
+++ hdk/inc/hdScripting.h
@@ -0,0 +1,16 @@
+/*
+ *  hdScripting.h
+ *  hdk
+ *
+ *  Created by David Petrie on 26/04/10.
+ *  Copyright 2010 n/a. All rights reserved.
+ *
+ */
+
+#ifndef _HD_SCRIPTING_H_
+#define _HD_SCRIPTING_H_
+
+#import "hdScripts/hdScripts.h"
+
+
+#endif
